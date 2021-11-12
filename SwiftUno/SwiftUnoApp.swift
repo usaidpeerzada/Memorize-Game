3ddd7211e8cd4a2d8ac5ef06b497896e34d574e6
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2021-11-13 01:55:41 +0530

    Initial Commit

---
+++ SwiftUno/SwiftUnoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUnoApp.swift
+//  SwiftUno
+//
+//  Created by Usaid Peerzada on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUnoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
